f93dc06f07e7564ae541d2e51ab697a99edcc9b4
JorgeRSev <jorge.rosado7@hotmail.com>
2025-03-17 19:18:31 +0100

    Añado el componente de card al componente userlist y muestro los usuarios

--- src/app/pages/home/user-list/user-list.component.ts
+++ src/app/pages/home/user-list/user-list.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { IRes, IUser } from '../../../interfaces/iuser.interface';
+import { UsersService } from '../../../services/users.service';
+import { UserCardComponent } from '../../../components/user-card/user-card.component';
 
 @Component({
   selector: 'app-user-list',
-  imports: [],
+  imports: [UserCardComponent],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.css'
 })
 export class UserListComponent {
+res!: IRes
+usersServices = inject(UsersService)
+arrUsers: IUser[] = []
 
+
+  ngOnInit(){
+    this.getUsers()
+  }
+
+  async getUsers(){
+    this.res = await this.usersServices.getAll()
+    console.log(this.res)
+    this.arrUsers = this.res.results;
+    console.log('Estoy en la funcion obtener usuarios en users-list', this.arrUsers)
+  }
 }
