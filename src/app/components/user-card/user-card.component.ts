60e9224b469bf5f1ab2da3b797267d4737878255
JorgeRSev <jorge.rosado7@hotmail.com>
2025-03-17 20:00:28 +0100

    Añado Comp. Buttons a las cards y le paso la informacion del usuario

--- src/app/components/user-card/user-card.component.ts
+++ src/app/components/user-card/user-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject, Input} from '@angular/core';
 import { UsersService } from '../../services/users.service';
 import { IUser } from '../../interfaces/iuser.interface';
+import { ButtonsComponent } from "../../shared/buttons/buttons.component";
 
 @Component({
   selector: 'app-user-card',
-  imports: [],
+  imports: [ButtonsComponent],
   templateUrl: './user-card.component.html',
   styleUrl: './user-card.component.css'
 })
