e13c3a43b1c2d2cb1c8a183145c5ee122126a871
JorgeRSev <jorge.rosado7@hotmail.com>
2025-03-21 19:20:08 +0100

    funcionalidad de peticiones en service y comprobar que funciona

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -18,21 +18,34 @@ export class UsersService {
     return lastValueFrom(this.httpClient.get<IUser>(`${this.endPoint}/${id}`));
   }
 
+  create(user: IUser): Promise<IUser>{
+    console.log('Estoy en el create USerDAta: ',user)
+    return lastValueFrom(this.httpClient.post<IUser>(this.endPoint, user))
+  //   .then(createUser => {
+  //     console.log('Usuario añadido: ', createUser);
+  //     return createUser;
+  // })
+  }
+
+  delete(id: string): Promise<IUser>{
+    return lastValueFrom(this.httpClient.delete<IUser>(`${this.endPoint}/${id}`))
+
+    // // Prueba para visualizar el usuario eliminado
+
+    // .then(deleteUser => {
+    //   console.log('Usuario eliminado: ', deleteUser);
+    //   return deleteUser;
+    // })
+  }
 
   update(user: IUser): Promise<IUser> {
     let { _id, id, ...userData } = user;
     console.log('Estoy en el update USerDAta: ',userData)
     return lastValueFrom(this.httpClient.put<IUser>(`${this.endPoint}/${_id}`, userData))
+  //   .then(updatedUser => {
+  //     console.log('Nuevos datos del usuario: ', updatedUser);
+  //     return updatedUser;
+  // })
   }
-
-  create(user: IUser): Promise<IUser>{
-    console.log('Estoy en el create USerDAta: ',user)
-    return lastValueFrom(this.httpClient.post<IUser>(this.endPoint, user))
-  }
-
-  delete(id: string): Promise<IUser>{
-    return lastValueFrom(this.httpClient.delete<IUser>(`${this.endPoint}/${id}`))
-  }
-
 
 }
