30d30f9815a50fdb3be1beeeadb671f846a639e2
JorgeRSev <jorge.rosado7@hotmail.com>
2025-03-21 19:18:19 +0100

    añadir funcionalidad al deleteUser y navigate a lista de usuarios

--- src/app/shared/buttons/buttons.component.ts
+++ src/app/shared/buttons/buttons.component.ts
@@ -1,7 +1,8 @@
 import { Component, inject, Input } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { IUser } from '../../interfaces/iuser.interface';
 import { UsersService } from '../../services/users.service';
+import { toast } from 'ngx-sonner';
 
 @Component({
   selector: 'app-buttons',
@@ -12,7 +13,20 @@ import { UsersService } from '../../services/users.service';
 export class ButtonsComponent {
   @Input() miUsuario: IUser | any;
   userServices = inject(UsersService)
+  router = inject(Router)
+  @Input() volver: Boolean = false;
 
-  deleteUser(id: string){}
+  deleteUser(id: string){
+    toast(`Vas a borrar al empleado ${this.miUsuario.first_name} ${this.miUsuario.last_name} `, {
+      action: {
+        label: 'Aceptar',
+        onClick: async () => {
+          await this.userServices.delete(id)
+          this.router.navigate(['/home', 'users'])
+
+        }
+      }
+    });
+  }
 
 }
