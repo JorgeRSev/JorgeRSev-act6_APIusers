546ee685e5319e7616b6441bd8f04674f9f30204
JorgeRSev <jorge.rosado7@hotmail.com>
2025-03-21 19:30:06 +0100

    mostrar form en base al id o vacio, recogida de datos del form.

--- src/app/pages/home/user-form/user-form.component.ts
+++ src/app/pages/home/user-form/user-form.component.ts
@@ -2,6 +2,8 @@ import { Component, inject, Input } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { UsersService } from '../../../services/users.service';
 import { IUser } from '../../../interfaces/iuser.interface';
+import { toast } from 'ngx-sonner';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-form',
@@ -10,35 +12,53 @@ import { IUser } from '../../../interfaces/iuser.interface';
   styleUrl: './user-form.component.css'
 })
 export class UserFormComponent {
-
+  userServices = inject(UsersService)
   miUsuario!: IUser
   @Input() idUser: string = ""
-  userServices = inject(UsersService)
   title: string = "Nuevo"
   userForm: FormGroup = new FormGroup({},[]);
+  router = inject(Router)
 
   async ngOnInit() {
     if (this.idUser){
+      try{
       this.miUsuario = await this.userServices.getById(this.idUser)
-      console.log('Estoy en el ngOnInit del formulario de usuario',this.miUsuario)
+      // console.log('Estoy en el ngOnInit del formulario de usuario',this.miUsuario)
       this.title = "Actualizar"
+      } catch(msg: any){
+        toast.error(msg.error)
+        console.log(msg)
+      }
     }
 
     this.userForm = new FormGroup({
     _id: new FormControl(this.idUser || null, []),
-    first_name: new FormControl(this.miUsuario.first_name || "", []),
-    last_name: new FormControl(this.miUsuario.last_name || "", []),
-    email: new FormControl(this.miUsuario.email || "", []),
-    image: new FormControl(this.miUsuario.image || "", [])
+    id: new FormControl(this.miUsuario?.id || 0, []),
+    first_name: new FormControl(this.miUsuario?.first_name || "", []),
+    last_name: new FormControl(this.miUsuario?.last_name || "", []),
+    email: new FormControl(this.miUsuario?.email || "", []),
+    image: new FormControl(this.miUsuario?.image || "", []),
+    username: new FormControl(this.miUsuario?.username || "", []),
+    password: new FormControl(this.miUsuario?.password || Math.random().toString().slice(2, 10), [])
     });
-    
   }
 
 
-  updateUser(){}
-
-
-
-
+  async getDataUser(){
+    let res: IUser | any
+    try{
+      if(this.userForm.value._id){
+        console.log('Estoy en el getDataUser del formulario de usuario',this.userForm.value)
+        res = await this.userServices.update(this.userForm.value)
+      } else{
+        console.log('Estoy en el getDataUser del formulario de usuario')
+        res = await this.userServices.create(this.userForm.value)    
+      }
+      // this.router.navigate(['/home', 'users'])
+    }catch(msg: any){
+      toast.error(msg.error)
+      console.log(msg)
+    }
+  }
 }
 
