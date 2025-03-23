7da267df6c8fb01c531f795c2dfe93cac6961d64
JorgeRSev <jorge.rosado7@hotmail.com>
2025-03-23 22:44:54 +0100

    añado sweetAlert para el borrado de usuario

--- src/app/shared/buttons/buttons.component.ts
+++ src/app/shared/buttons/buttons.component.ts
@@ -3,6 +3,7 @@ import { Router, RouterLink } from '@angular/router';
 import { IUser } from '../../interfaces/iuser.interface';
 import { UsersService } from '../../services/users.service';
 import { toast } from 'ngx-sonner';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-buttons',
@@ -16,17 +17,28 @@ export class ButtonsComponent {
   router = inject(Router)
   @Input() volver: Boolean = false;
 
-  deleteUser(id: string){
-    toast(`Vas a borrar al empleado ${this.miUsuario.first_name} ${this.miUsuario.last_name} `, {
-      position: "top-center",
-      action: {
-        label: 'Aceptar',
-        onClick: async () => {
-          await this.userServices.delete(id)
-          this.router.navigate(['/home', 'users'])
+  deleteUser(id: string) {
+    Swal.fire({
+      title: `¿Deseas eliminar a ${this.miUsuario.first_name} ${this.miUsuario.last_name}?`,
+      showCancelButton: true,
+      confirmButtonText: 'Aceptar',
+      cancelButtonText: 'Cancelar',
+      confirmButtonColor: '#d33',
+      cancelButtonColor: '#000',
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        try {
+          await this.userServices.delete(id);
+          this.router.navigate(['/home', 'users']);
+          Swal.fire(
+            'El usuario ha sido eliminado.'
+          );
+        } catch (error) {
+          Swal.fire(
+            'Hubo un problema al eliminar al usuario.'
+          );
         }
       }
     });
   }
-
 }
